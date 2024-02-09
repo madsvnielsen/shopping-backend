a2ae1d7685635179fde997e7edb180e756469e8b
Mads Nielsen <55346271+madsvnielsen@users.noreply.github.com>
2024-02-09 11:59:51 +0100

    Tags

--- src/routes/BasketRouter.ts
+++ src/routes/BasketRouter.ts
@@ -6,7 +6,7 @@ export const basketRouter = express.Router();
 
 basketRouter.post("/", (req : Request, res : Response) => {
     // #swagger.summary = 'Add item to basket'
-
+    // #swagger.tags = ["Basket"]
     // TODO:
     return res.send("Not yet implemented")
 
@@ -16,6 +16,7 @@ basketRouter.post("/", (req : Request, res : Response) => {
 
 basketRouter.delete("/:item_id", (req : Request<{card_id : string}>, res : Response) => {
     // #swagger.summary = 'Remove item from basket'
+    // #swagger.tags = ["Basket"]
 
     // TODO:
     return res.send("Not yet implemented")
@@ -23,18 +24,22 @@ basketRouter.delete("/:item_id", (req : Request<{card_id : string}>, res : Respo
 
 basketRouter.delete("/", (req : Request, res : Response) => {
     // #swagger.summary = 'Delete basket'
+    // #swagger.tags = ["Basket"]
     // TODO:
     return res.send("Not yet implemented")
 })
 
 basketRouter.post("/order", (req : Request, res : Response) => {
     // #swagger.summary = 'Place order'
+    // #swagger.tags = ["Basket"]
     // TODO:
     return res.send("Not yet implemented")
 })
 
 basketRouter.get("/order/receipt", (req : Request, res : Response) => {
+
     // #swagger.summary = 'Get receipt  basket'
+    // #swagger.tags = ["Basket"]
     // #swagger.description = 'Maybe this should be post idk?'
 
     // TODO:
--- src/routes/ProductsRouter.ts
+++ src/routes/ProductsRouter.ts
@@ -5,18 +5,21 @@ import {Card} from "../Models/CardModel";
 
 productsRouter.get("/list", (req : Request, res : Response) => {
     // #swagger.summary = 'List cards'
+    // #swagger.tags = ["Products"]
     // TODO:
     return res.send("Not yet implemented")
 })
 
 productsRouter.get("/search", (req : Request<{query : string}>, res : Response) => {
     // #swagger.summary = 'Search cards from string'
+    // #swagger.tags = ["Products"]
     // TODO:
     return res.send("Not yet implemented")
 })
 
 productsRouter.get("/:id", (req : Request<{id : string}>, res : Response) => {
     // #swagger.summary = 'Get card details'
+    // #swagger.tags = ["Products"]
     PokemonAPI.getPokemonCard(req.params.id).then((result : Card) => {
         res.send(result);
     })

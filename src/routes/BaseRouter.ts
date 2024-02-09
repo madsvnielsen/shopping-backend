8ed7b6a6462f2107aa0f57752aa7561c2fc3f67e
Magnus <magnusrk90@gmail.com>
2024-02-09 11:24:20 +0100

    Added the pokemon API

--- src/routes/BaseRouter.ts
+++ src/routes/BaseRouter.ts
@@ -1,4 +1,6 @@
 import express, { Express, Request, Response } from "express";
+import {PokemonAPI} from "../PokemonAPI/PokemonCards";
+import {Card} from "../Models/CardModel";
 
 export const baseRouter = express.Router();
 
@@ -8,4 +10,11 @@ export const baseRouter = express.Router();
 baseRouter.get('/test/', (req : Request, res : Response) =>{
     console.log("KEY: " + process.env.API_KEY)
     res.send(process.env.API_KEY);
+});
+
+
+baseRouter.get('/card/', (req : Request, res : Response) =>{
+    PokemonAPI.getPokemonCard('xy1-1').then((result : Card) => {
+        res.send(result);
+    })
 });

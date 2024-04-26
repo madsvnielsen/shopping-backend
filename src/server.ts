10d4b5a83688e8b226c790ceb7f1bf153b43c83b
Mads Nielsen <55346271+madsvnielsen@users.noreply.github.com>
2024-04-26 11:05:19 +0200

    Fix tests

--- src/server.ts
+++ src/server.ts
@@ -4,23 +4,16 @@ import * as swaggerDocument from './ApiDocs/swagger.json';
 import {baseRouter} from "./routes/BaseRouter";
 import {ShoppingDb} from "./database/ShoppingDb"
 import cors from "cors"
-import fs from 'fs'
-import http from 'http'
-import https from 'https'
-import { start } from "repl";
 
 
 export const app : Express = express();
-
-
-
-
-
-
-//export const server = app.listen(3000, () => console.log('Shopping backend is listening on port 3000.'));
-
 app.use(cors())
 ShoppingDb.initialize();
+
+
+export const server = app.listen(3000, () => console.log('Shopping backend is listening on port 3000.'));
+
+
 app.use("/", baseRouter)
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.get("/dbtest", (req : Request, res : Response) => {
@@ -29,15 +22,11 @@ app.get("/dbtest", (req : Request, res : Response) => {
     // #swagger.tags = ["Test"]
     // #swagger.description = 'Shows if the db is authentication'
     ShoppingDb.sequelize.authenticate().then(() => {
-        res.send('Connection has been established successfully.')
+          res.send('Connection has been established successfully.')
     }).catch((error: Error) => {
         res.send('Unable to connect to the database: ' + error);
     })
 
+
+
 })
-
-var httpServer = http.createServer(app);
-httpServer.listen(3000);
-
-
-
